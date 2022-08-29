870a4d5788d32a3602defccf09451b05516754fc
Patrik <patrik@qmusicplayer.se>
2022-08-29 21:21:52 +0200

    updated to pretty app and basic background interval

--- app.js
+++ app.js
@@ -2,142 +2,238 @@
 
 // https://mdn.github.io/dom-examples/service-worker/simple-service-worker/
 
-function log(msg,e){
-	if(e){
-		if(e instanceof Error){
-			console.warn(msg);
-			console.error(e);
-		}else{
-			console.log(msg,e);
-		}
-	}else{
-		console.log(msg);
-	}
-	let target=document.getElementById('log');
-	if(target){
-		let div=document.createElement('div');
-		div.innerText=msg;
-		target.appendChild(div);
-	}else{
-		console.warn("DOM not ready yet")
-	}
+// navigator.serviceWorker.ready.then((reg)=>console.warn('the service worker registration is now ready',reg));
+//^this fires when you press install...
+
+/* SERVICE WORKERS */
+var service=null;
+
+function isServiceSupported(){
+	return 'serviceWorker' in navigator; 
 }
-
-
-function notify(msg){
+function isServiceRunning(){
+	return isServiceSupported() && navigator.serviceWorker.controller;
+}
+async function isServiceRegistered(){
 	try{
-		log(msg);
-		let options={icon:"/favicon.ico"};
-		if ('serviceWorker' in navigator){
-			options.tag='backgroundNotification';
-			navigator.serviceWorker.ready.then(registration=>registration.showNotification(msg,options))
+		if(!'serviceWorker' in navigator){
+			return false;
 		}else{
-			options.tag='foregroundNotification';
-			new Notification(msg, options);
+			let registration=await Promise.race([
+				new Promise(res=>setTimeout(()=>res(false),100))
+				,navigator.serviceWorker.ready
+			])
+			return registration && registration.active ? true : false
 		}
 	}catch(e){
 		console.error(e);
-	} 
-	
-}
-
-
-
-function unregisterClearAndReload(){
-	log('unregistering service worker, clearing cache and reloading page...');
-	navigator.serviceWorker.ready
-		.then(registration=>registration.unregister())
-		.then(()=>location.reload(true))
-}
-
-function sendFiveNotices(){
-	notify("Notifying 5 times...");
-	var i=5
-	while(i){
-		let msg='Hello world! '+i;
-		i--;
-		let delay=i*1000;
-		// console.log(msg,delay);
-		setTimeout(()=>notify(msg),delay);
+		return false;
 	}
 }
 
 
-
-function enableNotifications(){
-	if(Notification.permission==='denied'){
-		log("Notifications have already been denied");
+/**
+ * Check if service workers are supported and if one is currently active and ready to be used.
+ * Also show the state next to the install button
+ * 
+ * NOTE: checks async if a service is registered but not running. will update dom but doesn't affect return value
+ * 
+ * @return boolean          True if a running service worker exists, else false
+ * @sync
+ */
+function checkService(){
+	let elem=document.querySelector('#install>.service .state');
+	let button=document.querySelector('#install>.service .enable');
+	if (!isServiceSupported()) {
+		showState(elem,'cross',"serviceWorkers are not supported by your browser");
+		button.disabled=true;
+		return false;
+	}else if(isServiceRunning()){
+		showState(elem,'checkmark',"A serviceWorker is running in the background");
+		button.disabled=true;
+		return true;
 	}else{
-		log("Enabling notifications...");
-		Notification.requestPermission(status=>{
-			log("Accept notifications? "+status);
-		})
-	}
-}
-
-function installApp(){
-	
-}
-
-function initNotifications(){
-
-	log("initializing notifications...");
-	try{
-		if(Notification){
-			// log("Notifications ARE supported");
-			if(Notification.permission==='granted'){
-				log("Notifications already granted");
-				blockButton();
-			}else if(Notification.permission==='denied'){
-				log("Notifications ARE supported but have already been denied");
-				console.log(Notification);
+		//supported, but we don't know if it's been installed but inactive
+		showState(elem,'nothing');
+		isServiceRegistered().then(reg=>{
+			if(reg){
+				button.disabled=true;
+				showState(elem,'info',"A serviceWorker has been registered but it not yet running, reload browser to run");
+			}else{
+				button.disabled=false;
+				console.log("serviceWorkers are supported, but you havn't installed one yet")
 			}
-		}else{
-			blockButton();
-			log("Notifications are NOT supported");
-		}
-
-	}catch(e){
-		log(e.message);
-		blockButton();
+		})
+		return false;
 	}
 }
 
-function blockButton(){
-	document.getElementById('grant').disabled=true;
-}	
-	 
 
 
+function Service(){
+	//Will be set to true when you can send
+	this.connected=false;
+	var onconnect;
+	this.connect=()=>{
+		if(!isServiceRunning()){
+			console.error("service worker not running");
+			this.connected=false;
+			return Promise.resolve(false);
+		}else if(this.connected){
+			console.warn("socket already connected to service");
+			return Promise.resolve(true)
+		}else{
+			console.log("setting up MessageChannel to service...");
+			//We can talk to the service worker using the .postMessage() method, but the service worker can't
+			//respond unless we give him a means of doing so, enter MessageChannels.
+			const channel = new MessageChannel();
 
-async function registerServiceWorker(){
+			//The channel has 2 ports. 
+			//We keep port1 and listen to it for responses messages from the service
+			channel.port1.onmessage=receive;
+			//We transfer ownership of port2 to the service which it can use to respond to us
+			navigator.serviceWorker.controller.postMessage('REGISTER', [channel.port2]);
+
+			return new Promise(res=>{onconnect=res});
+		}
+	}
+
+	const pending={};
+	var id=0;
+	this.send=(method,payload)=>{
+		if(!this.connected)
+			return Promise.reject("not connected");
+
+		try{
+			let msgId=id++;
+			let msg={method,msgId,payload};
+			let resolve;
+			let promise=new Promise((res)=>{resolve=res});
+			pending[msgId]=resolve;
+			navigator.serviceWorker.controller.postMessage(msg)
+			return promise;
+		}catch(e){
+			return Promise.reject(e);
+		}
+	}
+	this.backgroundNotification=(msg,delay)=>{
+		this.send('showNotification',{msg,delay}).catch(console.error);
+	}
+
+	//This promise will resolve with this object when the channel is ready for use, see reveice()
+	
+
+	const receive=(event)=>{
+		try{	
+		  if(event.data.msgId){
+		  	if(pending.hasOwnProperty(event.data.msgId)){
+				pending[event.data.msgId](event.data.err,event.data.response);
+		  	}else{
+		  		console.warn("msgId="+event.data.msgId+" doesn't match any pending messages",pending,event.data.payload);
+		  	}
+		  }else if(event.data=='REGISTERED'){
+		  	this.connected=true;
+		  	if(typeof onconnect=='function')
+		  		onconnect(true);
+		  }else{
+		  	console.warn('Unknown message from service:',event);
+		  }
+		}catch(e){
+			console.error(e);
+		}
+	}
+
+}
+
+
+async function enableService(){
 	try{
-		// log("initializing service worker...");
-
-		if (!'serviceWorker' in navigator) {
-			throw new Error("serviceWorker not supported");
+		let elem=document.querySelector('#install>.service .state');
+		let button=document.querySelector('#install>.service .install');
+		if (!isServiceSupported()) {
+			showState(elem,'cross',"serviceWorkers are not supported by your browser");
+			button.disabled=true;
+		}else if(isServiceRunning()){
+			showState(elem,'checkmark',"background service is already running");
+			button.disabled=true;
 		}else{
-			// log("service workers are supported")
-		}
+			var registration;
+			if(await isServiceRegistered()){
+				showState(elem,'info',"service worker already registered");
+				registration=await navigator.serviceWorker.ready;
+			}else{
+				showState(elem,'info',"registering service worker...");
+				registration=await navigator.serviceWorker.register('/serviceworker.js',{scope: '/'})
+				await sleep(500);
+			}
 
-		let registration=await navigator.serviceWorker.register('/serviceworker.js',{scope: '/'})
-		
-		if (registration.installing) {
-			log("Service worker being installed on this load...");
-		} else if (registration.waiting) {
-			log("Service worker is 'installed'... unsure what that means...",registration)
-		} else if (registration.active) {
-			log("Previous service worker found and is already running")
+			registration.update(); //check server for new version
+			checkService();
+		}
+	}catch(e){
+		console.error(e);
+	}
+}
+
+async function disableService(){
+	try{
+		if(await isServiceRegistered()){
+			if(service && service.connected){
+				console.log('clearing background intervals...');
+				await service.send('clearAllBackgroundIntervals');
+			}
+			console.log('unregistering service worker...');
+			let registration=await navigator.serviceWorker.getRegistration();
+			await registration.unregister();
+			await sleep(500);
+			showState('#install>.service .state','info','You have to reload the browser to complete the uninstall')
+		}else{
+			console.log("no service worker registered");
 		}
 
 	}catch(e){
-		log("Failed to register service worker",e);
+		console.error(e);
 	}
-	return;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* INSTALL POPUP */
 
 function showPopup(){
+	checkService();
+	checkNotifications();
 	document.getElementById('overlay').classList.remove('hidden');
 }
 function hidePopup(elem,evt){
@@ -145,12 +241,192 @@ function hidePopup(elem,evt){
 		document.getElementById('overlay').classList.add('hidden');
 	}
 }
+function showState(elem,show,tooltip){
+	try{
+		if(show=='cross')
+			console.warn(tooltip);
+		else if(tooltip)
+			console.log(tooltip);
+		
+		if(typeof elem=='string')
+			elem=document.querySelector(elem) || document.getElementById(elem);
+		
+		if(!elem || !elem.classList){
+			console.error("Not valid element:",elem,show,tooltip);
+		}else{
+			elem.classList.remove('checkmark');
+			elem.classList.remove('cross');
+			elem.classList.remove('info');
+			if(show && show!='nothing'){
+				elem.classList.add(show);
+			}
+			elem.title=tooltip;
+		}
 
-
-function init(){
-	log("Running app.js..."+(new Date()).toUTCString())
-	// registerServiceWorker();
-	// initNotifications();
-	console.log('app.js init() ran to end',self,document,window);
+	}catch(e){
+		console.error(e);
+	}
 }
 
+
+
+
+
+
+
+
+/* NOTIFICATIONS */
+
+function checkNotifications(){
+	var perm;
+	if(Notification){
+		perm=Notification.permission;
+	}else{
+		perm='unsupported';
+	}
+	let elem=document.querySelector('#install>.notification .state');
+	switch(Notification.permission){
+		case 'default':
+			showState(elem,'nothing');
+			return 'supported';
+		case 'granted':
+			showState(elem,'checkmark',"Notifications have been enabled");
+			break;
+		case 'denied':
+			showState(elem,'cross','You have denied notifications from being shown');
+			break;
+		case 'unsupported':
+			showState(elem,'cross','Notifications are not supported by your browser');
+			break;
+		default:
+			showState(elem,'cross','unknown notification permission: '+Notification.permission);
+			perm='unsupported';
+	}
+	document.querySelector('#install>.notification .enable').disabled=true;
+	return perm;
+}
+
+function enableNotifications(){
+	if(checkNotifications()=='supported'){
+		Notification.requestPermission(checkNotifications);
+	}else{
+		console.warn("Not enabling notifications");
+	}
+}
+
+
+function sendNotification(msg,delay){
+	if(checkNotifications()!='granted'){
+		console.warn("Not sending notification");
+		return;
+	}
+	delay=(delay||0);
+
+	if (service && service.connected){
+		service.backgroundNotification(msg,delay);
+	}else{
+		console.log('NOTIFY*:',msg);
+		setTimeout(()=>{
+			try{
+				new Notification(msg, {icon:"/favicon.ico",tag:'foregroundNotification'});
+			}catch(e){
+				console.error(e);
+			} 	
+		},delay);
+	}
+
+
+}
+
+
+function demoNotification(){
+	if(service && service.connected){
+		const elem=document.querySelector('#install>.notification .state');
+		var i=5
+		sendNotification('This notification was fired from the background service. It will fire even if '+
+			"your browser isn't open! Try it!",i*1000);
+		elem.innerText=i;
+		const interval=setInterval(()=>{
+			i--;
+			if(i<1){
+				elem.innerText="";
+				clearTimeout(interval);
+			}else{
+				elem.innerText=i;
+			}
+		},1000)
+	}else{
+		sendNotification('This notification was fired right away because you have not installed the'+
+			' background service');
+	}
+}
+
+
+
+
+
+
+
+
+function hardReload(){
+	disableService().then(()=>{
+		console.log("clearing cache and reloading page")
+		location.reload(true)
+	});
+}
+
+function sleep(ms){
+	return new Promise(resolve=>{
+		setTimeout(resolve,ms);
+	})
+}
+
+async function showAppContent(){
+	try{
+		//Change text
+		let welcome=document.querySelector('#welcome');
+		welcome.classList.add('fade-out');
+		await sleep(1000);
+		welcome.innerHTML='Welcome to the Paragast <span class="txt-purple">App</span>';
+		welcome.classList.add('fade-in');
+		welcome.classList.remove('fade-out');
+		await sleep(300);
+
+		//show button
+		let btn=document.querySelector('main button.hidden')
+		btn.classList.add('fade-in');
+		btn.classList.remove('hidden');
+	}catch(e){
+		console.error(e);
+	}
+}
+
+
+
+
+async function initApp(){
+	try{
+		if(!isServiceRunning()){
+			console.warn("Not running app because service is not running");
+		}else{
+			//connect to service
+			service=new Service();
+			await service.connect();
+			
+			//Announce we're running in background
+			service.backgroundNotification('Paragast is running in the background',1000);
+			
+			showAppContent();
+
+		}
+	}catch(e){
+		console.error(e);
+	}
+}
+
+
+function demoApp(){
+	if(service && service.connected){
+		service.send('notificationInterval',{msg:"Paragast is still running",interval:60000})
+	}
+}
